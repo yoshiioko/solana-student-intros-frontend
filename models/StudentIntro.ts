00ba5e9ed9680360317aa5915814595161dffc72
yoshiioko <acardoza@pdx.edu>
2022-12-17 15:03:05 -0800

    create StudentIntro schema and implement serialize function for it.

--- models/StudentIntro.ts
+++ models/StudentIntro.ts
@@ -1,15 +1,35 @@
+import * as borsh from "@project-serum/borsh";
+
 export class StudentIntro {
-    name: string;
-    message: string;
+  name: string;
+  message: string;
 
-    constructor(name: string, message: string) {
-        this.name = name;
-        this.message = message;
-    }
+  constructor(name: string, message: string) {
+    this.name = name;
+    this.message = message;
+  }
 
-    static mocks: StudentIntro[] = [
-        new StudentIntro('Elizabeth Holmes', `Learning Solana so I can use it to build sick NFT projects.`),
-        new StudentIntro('Jack Nicholson', `I want to overhaul the world's financial system. Lower friction payments/transfer, lower fees, faster payouts, better collateralization for loans, etc.`),
-        new StudentIntro('Terminator', `i'm basically here to protect`),
-    ]
+  static mocks: StudentIntro[] = [
+    new StudentIntro(
+      "Elizabeth Holmes",
+      `Learning Solana so I can use it to build sick NFT projects.`
+    ),
+    new StudentIntro(
+      "Jack Nicholson",
+      `I want to overhaul the world's financial system. Lower friction payments/transfer, lower fees, faster payouts, better collateralization for loans, etc.`
+    ),
+    new StudentIntro("Terminator", `i'm basically here to protect`),
+  ];
+
+  studentIntroSchema = borsh.struct([
+    borsh.u8("variant"),
+    borsh.str("name"),
+    borsh.str("message"),
+  ]);
+
+  serialize(): Buffer {
+    const buffer = Buffer.alloc(1000);
+    this.studentIntroSchema.encode({ ...this, variant: 0 }, buffer);
+    return buffer.slice(0, this.studentIntroSchema.getSpan(buffer));
+  }
 }
